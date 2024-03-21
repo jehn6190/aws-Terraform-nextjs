05bc2f8d859e2aad32c84113bcd4f1d374a0ecac
Ndukwe Onyinyechi <ljenclade@yahoo.com>
2024-03-21 19:10:59 +0000

    git cache cleared

--- main.tf
+++ main.tf
@@ -95,10 +95,10 @@ resource "aws_security_group" "webserver" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags = {
-    Name = "Allow traffic"
-  }
-}
+#   tags = {
+#     Name = "Allow traffic"
+#   }
+# }
 
 resource "aws_instance" "web" {
   ami                    = var.amis[var.region]
